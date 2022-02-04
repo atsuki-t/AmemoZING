d078418c65388ee2bbfb5853f52f447144f87994
atsuki-t <atsuki-t@weseek.co.jp>
2022-02-04 20:08:33 +0900

    add middleware mongodb.ts

---
+++ middleware/mongodb.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+if (!MONGODB_URI) {
+  throw new Error(
+    '.env.local ファイルの中で MONGODB_URI の変数を定義してください。'
+  )
+}
+
+const connectDB = async() => {
+  await mongoose.connect(MONGODB_URI);
+}
+
+export default connectDB;
