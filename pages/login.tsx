20bf47a8199ed0028ba274685b9bc5293f7d3b3a
atsuki-t <atsuki-t@weseek.co.jp>
2022-02-05 18:18:30 +0900

    create login form

--- pages/login.tsx
+++ pages/login.tsx
@@ -13,13 +13,26 @@ const Login: React.FC = () => {
   }
 
   return (
-    <>
-      <h1>Login</h1>
-      <button onClick={login}>ログイン</button>
-      <div>
-        <Link href="/"><a>Homeへ</a></Link>
+    <div className="vh-100 d-flex align-items-center">
+      <div className="container">
+        <div className="card bg-light p-5 mx-5">
+          <p className="fs-3 text-center mb-4">ログイン</p>
+
+          <div className="mb-4">
+            <label htmlFor="usernameInput" className="form-label">ユーザー名</label>
+            <input className="form-control" id="usernameInput"></input>
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="passwordInput" className="form-label">パスワード</label>
+            <input className="form-control" id="passwordInput"></input>
+          </div>
+
+          <button onClick={login} className='btn btn-primary mb-3'>ログイン</button>
+          <Link href="/"><a className="text-primary">アカウント新規登録はこちら</a></Link>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
