ebe689c6b04bba649c00a77042e426fe095f0a12
atsuki-t <atsuki-t@weseek.co.jp>
2022-02-06 18:46:29 +0900

    fix auth.js

--- components/auth.tsx
+++ components/auth.tsx
@@ -1,30 +1,34 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Cookies from 'js-cookie'
 
 const Auth: React.FC = ({ children }) => {
+  const [showChild, setShowChild] = useState(false)
   const router = useRouter()
 
   useEffect(() => {
     // ログイン画面とアカウント作成画面では Cookie のチェックをしない
     if (router.pathname == '/login' || router.pathname == '/sign_up') {
+      setShowChild(true)
       return
     }
 
     // Cookie のチェック
     const signedIn = Cookies.get('signedIn')
 
-    // signedIn が true じゃなければ /login へ
-    if (signedIn !== 'true') {
+    // signedIn が true であれば画面を表示し、false であれば /login へ移動する
+    if (signedIn) {
+      setShowChild(true)
+    } else {
       router.replace('/login')
     }
-  })
+  }, [router])
 
-  return (
-    <>
-      {children}
-    </>
-  )
+  if (!showChild) {
+    return null
+  }
+
+  return <>{children}</>
 }
 
 export default Auth
