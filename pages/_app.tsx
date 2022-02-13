e7d77cfc70b354fc710c41f6ebccc1b03da00f43
atsuki-t <atsuki-t@weseek.co.jp>
2022-02-13 18:14:45 +0900

    fix layout show

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import Auth from '../components/auth'
-import Layout from '../components/layout'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Auth>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </Auth>
   )
 }
