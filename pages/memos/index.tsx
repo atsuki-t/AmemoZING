d4ab3d69d1c3e64caff54f685434328d605e0a3e
atsuki-t <atsuki-t@weseek.co.jp>
2022-02-20 20:13:21 +0900

    create memos tabel

--- pages/memos/index.tsx
+++ pages/memos/index.tsx
@@ -1,11 +1,36 @@
 import * as React from 'react'
-import { Container, Card } from '@material-ui/core'
+import axios from 'axios'
+import { Container, Card, Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core'
+
+type memoType = {
+  title: string
+  text: string
+}
 
 const Memos: React.VFC = () => {
+  let memos: Array<memoType> = []
+  axios.get('/api/memo', { params: { username: 'test' }
+  }).then((res) => {
+    memos = res.data
+  }).catch((err) => {
+    // データの取得に失敗した時の処理
+  })
+
   return (
     <Container maxWidth="lg">
       <Card className="p-4">
-        test
+        <Table aria-label="simple table">
+          <TableBody>
+            {memos.map((memo) => (
+              <TableRow key={memo.title}>
+                <TableCell component="th" scope="row">
+                  {memo.title}
+                </TableCell>
+                <TableCell align="right">{memo.text}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </Card>
     </Container>
   )
